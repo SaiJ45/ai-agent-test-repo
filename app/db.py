03e79ed410b4eb96049311f1c7916e85e240e675
SaiJ45 <saijawalkar787@gmail.com>
2026-03-26 16:04:03 +0530

    Fix #5

--- app/db.py
+++ app/db.py
@@ -1,14 +1,11 @@
 import sqlite3
 
 def connect():
-    return sqlite3.connect("test.db")
-
+    return sqlite3.connect('test.db')
 
 def get_user(user_id):
     conn = connect()
     cursor = conn.cursor()
-
-    query = f"SELECT * FROM users WHERE id = {user_id}"  # SQL injection risk
-    cursor.execute(query)
-
+    query = 'SELECT * FROM users WHERE id = ?'
+    cursor.execute(query, (user_id,))
     return cursor.fetchone()
